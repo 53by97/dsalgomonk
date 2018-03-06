1b4ada18fe4ae021a3bf095b86da4dedfbe0af45
srimanta.sahu <srimanta.sahu@spire2grow.com>
2018-03-07 04:23:51 +0530

    committed inorder, preorder, postorder tree traversals programs
    -- added towers of hanoi program

---
+++ src/main/java/com/kvvssut/dsalgomonk/generic/TowersOfHanoi.java
@@ -0,0 +1,19 @@
+package com.kvvssut.dsalgomonk.generic;
+
+public class TowersOfHanoi {
+
+	public static void main(String[] args) {
+		towersOfHanoi(3, 'A', 'C', 'B');
+	}
+
+	private static void towersOfHanoi(int n, char from, char to, char aux) {
+		if (n == 1) {
+			System.out.println("Move disk 1 form [" + from + "] to [" + to +"]");
+			return;
+		}
+		towersOfHanoi(n - 1, from, aux, to);
+		System.out.println("Move disk " + n + " form [" + from + "] to [" + to +"]");
+		towersOfHanoi(n - 1, aux, to, from);
+	}
+
+}
---
+++ src/main/java/com/kvvssut/dsalgomonk/utils/BinaryTree.java
@@ -0,0 +1,35 @@
+package com.kvvssut.dsalgomonk.utils;
+
+public class BinaryTree {
+	private int data;
+	private BinaryTree left, right;
+
+	public BinaryTree(int data) {
+		this.data = data;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public BinaryTree getLeft() {
+		return left;
+	}
+
+	public void setLeft(BinaryTree left) {
+		this.left = left;
+	}
+
+	public BinaryTree getRight() {
+		return right;
+	}
+
+	public void setRight(BinaryTree right) {
+		this.right = right;
+	}
+	
+}
